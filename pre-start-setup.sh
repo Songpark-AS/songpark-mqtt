75d835800d8194059127fe1bbb600993390698a8
Mathias Kjeldsberg Andersen <hemanngosser@gmail.com>
2021-10-18 16:41:16 +0200

    Use MQTT_USERNAME env var

--- pre-start-setup.sh
+++ pre-start-setup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 touch /mosquitto/config/passwordfile
-mosquitto_passwd -b /mosquitto/config/passwordfile songpark $MQTT_PASSWORD
+mosquitto_passwd -b /mosquitto/config/passwordfile $MQTT_USERNAME $MQTT_PASSWORD
 /usr/sbin/mosquitto -c /mosquitto/config/mosquitto.conf
