61e13b93cca660b70e458df075f6beaaa806d44d
Mathias Kjeldsberg Andersen <hemanngosser@gmail.com>
2021-10-18 15:14:05 +0200

    Use env variable for setting mqtt password

--- pre-start-setup.sh
+++ pre-start-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # TODO: create passwordfile with mosquitto_passwd here and put it in /mosquitto/config/passwordfile
 touch /mosquitto/config/passwordfile
-mosquitto_passwd -b /mosquitto/config/passwordfile songpark getTheRealPasswordHere
+mosquitto_passwd -b /mosquitto/config/passwordfile songpark $MQTT_PASSWORD
 /usr/sbin/mosquitto -c /mosquitto/config/mosquitto.conf
 
