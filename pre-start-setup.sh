437a996469bcfb3231ed8bb7de2e7f2b9abc0656
Mathias Kjeldsberg Andersen <hemanngosser@gmail.com>
2021-10-18 15:15:12 +0200

    remove todo comment

--- pre-start-setup.sh
+++ pre-start-setup.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-# TODO: create passwordfile with mosquitto_passwd here and put it in /mosquitto/config/passwordfile
 touch /mosquitto/config/passwordfile
 mosquitto_passwd -b /mosquitto/config/passwordfile songpark $MQTT_PASSWORD
 /usr/sbin/mosquitto -c /mosquitto/config/mosquitto.conf
-
